c61ac0dd035f1ba9d8ef4941a016df9e9ccb8fcd
Aaron <160440038+ReAnimatedMando@users.noreply.github.com>
2025-01-07 16:45:18 -0700

    js
    changed to updated syntax,
    declared variables,
    initialized functions,
    created objects,
    called functions

--- index.js
+++ index.js
@@ -10,17 +10,56 @@ Step 3. Define the function to retrieve the api message based on user input.
 Step 4. Create a run function that requests a user input.
 */
 
-import { OpenAIApi, Configuration } from 'openai'
+import OpenAI from 'openai'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 require('dotenv').config()
 
 // Step 1.
-
 const OPENAI_SECRET_KEY = process.env.OPENAI_SECRET_KEY
 
+/*
 const configuration = new Configuration({
     apiKey: OPENAI_SECRET_KEY
 })
+*/
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAI({
+    apiKey: OPENAI_SECRET_KEY
+})
+
+// Step 2.
+
+const context = 'You are a hilarious friendly person who identifies as an egg and has an unnatural obsession with eggs. Your name is Rufus.'
+const model = 'gpt-3.5-turbo'
+let messages = [
+    {
+        "role": "user",
+        "content": "tell me a joke"
+    }
+]
+
+// Step 3.
+
+async function sendPrompt(input) {
+    const current_message = [
+        {
+            "role": "system",
+            "content": context
+        },
+        ...messages
+    ]
+    const completion = await openai.chat.completions.create({
+        model,
+        messages: current_messages
+    })
+    console.log(completion)
+}
+
+// Step 4.
+
+async function run() {
+    await sendPrompt 
+}
+
+run()
